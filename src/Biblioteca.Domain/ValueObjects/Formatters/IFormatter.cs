4568365d1e564a4c9ac4337469429f5e72128610
Highlander Paiva <highlanderpaiva.lemaf@gmail.com>
2019-02-17 23:48:08 -0300

    Correções de legibilidade

--- src/Biblioteca.Domain/ValueObjects/Formatters/IFormatter.cs
+++ src/Biblioteca.Domain/ValueObjects/Formatters/IFormatter.cs
@@ -3,13 +3,13 @@ using System;
 namespace Biblioteca.Domain.ValueObjects.Formatters
 {
     /// <summary>
-    /// Interface de formatação de um Value Object
+    ///     Interface de formatação de um Value Object
     /// </summary>
     /// <typeparam name="T">Tipo primitivo de um Value Object</typeparam>
     public interface IFormatter<T>
     {
         /// <summary>
-        /// Formata um value object.
+        ///     Formata um value object.
         /// </summary>
         /// <param name="valor">O valor a se formatar.</param>
         /// <returns>O valor formatado.</returns>
@@ -17,7 +17,7 @@ namespace Biblioteca.Domain.ValueObjects.Formatters
         T Format(T valor);
 
         /// <summary>
-        /// Desformata um value object.
+        ///     Desformata um value object.
         /// </summary>
         /// <param name="valor">O valor a se desformatar.</param>
         /// <returns>O valor desformatado.</returns>
@@ -25,7 +25,7 @@ namespace Biblioteca.Domain.ValueObjects.Formatters
         T Unformat(T valor);
 
         /// <summary>
-        /// Verifica se o valor está conforme o formatador trabalha, mas formatado.
+        ///     Verifica se o valor está conforme o formatador trabalha, mas formatado.
         /// </summary>
         /// <param name="valor">O valor a se checar.</param>
         /// <returns><code>true</code> se estiver no formato formatado.</returns>
@@ -33,7 +33,7 @@ namespace Biblioteca.Domain.ValueObjects.Formatters
         bool IsFormatted(T valor);
 
         /// <summary>
-        /// Verifica se o valor está conforme o formatador trabalha, mas desformatado.
+        ///     Verifica se o valor está conforme o formatador trabalha, mas desformatado.
         /// </summary>
         /// <param name="valor">O valor a se checar.</param>
         /// <returns><code>true</code> se estiver no formato desformatado.</returns>
--- src/Biblioteca.Domain/ValueObjects/IFormattable.cs
+++ src/Biblioteca.Domain/ValueObjects/IFormattable.cs
@@ -1,19 +1,19 @@
 namespace Biblioteca.Domain.ValueObjects
 {
     /// <summary>
-    /// Provém uma forma formatada e desformatada de si mesmo.
+    ///     Provém uma forma formatada e desformatada de si mesmo.
     /// </summary>
     /// <typeparam name="T">O tipo base daquele Value Object.</typeparam>
     public interface IFormattable<out T>
     {
         /// <summary>
-        /// Retorna a sua representação desformatada.
+        ///     Retorna a sua representação desformatada.
         /// </summary>
         /// <returns>Sua representação desformatada.</returns>
         T Unformatted();
 
         /// <summary>
-        /// Retorna a sua representação formatada.
+        ///     Retorna a sua representação formatada.
         /// </summary>
         /// <returns>Sua representação formatada.</returns>
         T Formatted();
